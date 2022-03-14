94e02d989a809b718386ae6be418149322ec1105
Priyansh Singh <singhpriyansh2000@gmail.com>
2022-03-15 04:27:05 +0530

    added a navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <i className="logo-icon" class="fa fa-globe">
+        <span> my travel journal.</span>
+      </i>
+    </nav>
+  );
+};
+
+export default Navbar;
